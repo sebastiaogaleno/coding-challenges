6bad9e3ace54782fce13b0d8a1f5582cb8897ffd
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2023-08-29 17:13:26 -0300

    beecrowd - #1009 was solved

---
+++ beecrowd/1.iniciante/1009.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('../../stdin', 'utf8');
+const lines = input.split('\n');
+
+const salary = parseFloat(lines[1]);
+const sales = parseFloat(lines[2]);
+
+const total = salary + (sales * 0.15);
+
+console.log(`TOTAL = R$ ${total.toFixed(2)}`);
