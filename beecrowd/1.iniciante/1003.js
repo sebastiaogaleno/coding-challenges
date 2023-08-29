0e48b1b00f7f11e15432ed92b785ce2f42175cc4
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2023-08-29 16:39:36 -0300

    beecrowd - #1003 was solved

---
+++ beecrowd/1.iniciante/1003.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('../../stdin', 'utf8');
+const lines = input.split('\n').map(input => parseInt(input));
+
+const [a, b] = lines;
+
+console.log(`SOMA = ${a+b}`);
