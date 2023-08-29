fce3c1c4d96f2cac36f839043e1e2a79711d0662
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2023-08-29 17:02:03 -0300

    beecrowd - #1007 was solved

---
+++ beecrowd/1.iniciante/1007.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('../../stdin', 'utf8');
+const lines = input.split('\n').map(input => parseInt(input));
+
+const [a, b, c, d] = lines;
+const diferenca = a*b - c*d;
+
+console.log(`DIFERENCA = ${diferenca}`);
