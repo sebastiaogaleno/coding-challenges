58b4507ba4e04607fe8982f2aab2fa08d2105a5f
Sebastião Galeno <50140269+sebastiaogaleno@users.noreply.github.com>
2023-08-29 16:59:24 -0300

    beecrowd - #1006 was solved

---
+++ beecrowd/1.iniciante/1006.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('../../stdin', 'utf8');
+const lines = input.split('\n').map(input => parseFloat(input));
+
+const [a, b, c] = lines;
+const media = (a*2 + b*3 + c*5)/10;
+
+console.log(`MEDIA = ${media.toFixed(1)}`);
